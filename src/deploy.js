8348f28b99ea7e0997c908bf3825089cb3201a84
Felix <shenghuntianlang@outlook.com>
2024-08-23 17:02:26 +0800

    integration other actions toolkits

--- src/deploy.js
+++ src/deploy.js
@@ -7,52 +7,66 @@ const SAEClient = require('@alicloud/sae20190506');
 const OpenApi = require('@alicloud/openapi-client');
 const Util = require('@alicloud/tea-util');
 
-class Client {
+// class Client {
+//
+//     /**
+//      * Initialize the Client with the AccessKey of the account
+//      * @return Client
+//      * @throws Exception
+//      */
+//     static createClient() {
+//         // The project code leakage may result in the leakage of AccessKey, posing a threat to the security of all resources under the account. The following code examples are for reference only.
+//         // It is recommended to use the more secure STS credential. For more credentials, please refer to: https://www.alibabacloud.com/help/en/alibaba-cloud-sdk-262060/latest/credentials-settings-5.
+//         let config = new OpenApi.Config({
+//             // Required, please ensure that the environment variables ALIBABA_CLOUD_ACCESS_KEY_ID is set.
+//             accessKeyId: this.accessKeyId, // Required, please ensure that the environment variables ALIBABA_CLOUD_ACCESS_KEY_SECRET is set.
+//             accessKeySecret: this.accessKeySecret,
+//         });
+//         // See https://api.alibabacloud.com/product/sae.
+//         config.regionId = this.regionId;
+//         return new SAEClient.default(config);
+//     }
+//
+//     static async main() {
+//         this.accessKeyId = core.getInput('access-key-id', { required: false });
+//         this.accessKeySecret = core.getInput('access-key-secret', { required: false });
+//         this.regionId = core.getInput('region-id', { required: false });
+//         this.appId = core.getInput('app-id', { required: false });
+//         this.imageUrl = core.getInput('image-url', { required: false });
+//
+//         let client = Client.createClient();
+//         let deployApplicationRequest = new SAEClient.DeployApplicationRequest({});
+//         deployApplicationRequest.appId = this.appId;
+//         deployApplicationRequest.imageUrl = this.imageUrl;
+//         let runtime = new Util.RuntimeOptions({});
+//         let headers = { 'Content-Type': 'application/json' };
+//         try {
+//             // Copy the code to run, please print the return value of the API by yourself.
+//             await client.deployApplicationWithOptions(deployApplicationRequest, headers, runtime);
+//         } catch (error) {
+//             // Only a printing example. Please be careful about exception handling and do not ignore exceptions directly in engineering projects.
+//             // print error message
+//             console.log(error.message);
+//             // Please click on the link below for diagnosis.
+//             console.log(error.data.Recommend);
+//             Util.default.assertAsString(error.message);
+//         }
+//     }
+// }
 
-    /**
-     * Initialize the Client with the AccessKey of the account
-     * @return Client
-     * @throws Exception
-     */
-    static createClient() {
-        // The project code leakage may result in the leakage of AccessKey, posing a threat to the security of all resources under the account. The following code examples are for reference only.
-        // It is recommended to use the more secure STS credential. For more credentials, please refer to: https://www.alibabacloud.com/help/en/alibaba-cloud-sdk-262060/latest/credentials-settings-5.
-        let config = new OpenApi.Config({
-            // Required, please ensure that the environment variables ALIBABA_CLOUD_ACCESS_KEY_ID is set.
-            accessKeyId: this.accessKeyId, // Required, please ensure that the environment variables ALIBABA_CLOUD_ACCESS_KEY_SECRET is set.
-            accessKeySecret: this.accessKeySecret,
-        });
-        // See https://api.alibabacloud.com/product/sae.
-        config.regionId = this.regionId;
-        return new SAEClient.default(config);
-    }
+// Client.main().catch(e => core.setFailed(e));
 
-    static async main() {
-        this.accessKeyId = core.getInput('access-key-id', { required: false });
-        this.accessKeySecret = core.getInput('access-key-secret', { required: false });
-        this.regionId = core.getInput('region-id', { required: false });
-        this.appId = core.getInput('app-id', { required: false });
-        this.imageUrl = core.getInput('image-url', { required: false });
-
-        let client = Client.createClient();
-        let deployApplicationRequest = new SAEClient.DeployApplicationRequest({});
-        deployApplicationRequest.appId = this.appId;
-        deployApplicationRequest.imageUrl = this.imageUrl;
-        let runtime = new Util.RuntimeOptions({});
-        let headers = { 'Content-Type': 'application/json' };
-        try {
-            // Copy the code to run, please print the return value of the API by yourself.
-            await client.deployApplicationWithOptions(deployApplicationRequest, headers, runtime);
-        } catch (error) {
-            // Only a printing example. Please be careful about exception handling and do not ignore exceptions directly in engineering projects.
-            // print error message
-            console.log(error.message);
-            // Please click on the link below for diagnosis.
-            console.log(error.data.Recommend);
-            Util.default.assertAsString(error.message);
-        }
-    }
-
+async function main() {
+    const accessKeyId = core.getInput('access-key-id', { required: false });
+    const accessKeySecret = core.getInput('access-key-secret', { required: false });
+    const regionId = core.getInput('region-id', { required: false });
+    const appId = core.getInput('app-id', { required: false });
+    const imageUrl = core.getInput('image-url', { required: false });
+    console.log(accessKeyId);
+    console.log(accessKeySecret);
+    console.log(regionId);
+    console.log(appId);
+    console.log(imageUrl);
 }
 
-Client.main().catch(e => core.setFailed(e));
+main().catch(e => core.setFailed(e))
