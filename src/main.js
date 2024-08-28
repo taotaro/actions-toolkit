5c0248f32a7e50ca5b1d61766bd0fa53bc3f47b3
Felix <shenghuntianlang@outlook.com>
2024-08-28 18:19:12 +0800

    add lark notification

--- src/main.js
+++ src/main.js
@@ -9,7 +9,8 @@ class Client {
         this.title = core.getInput('title', { required: true });
         this.changes = core.getInput('changes', { required: true });
         this.trigger = core.getInput('trigger', { required: true });
-        this.duration = core.getInput('duration', { required: true });
+        this.startTime = new Date(core.getInput('start-time', { required: true }));
+        this.endTime = new Date(core.getInput('end-time', { required: true }));
         this.repository = core.getInput('repository', { required: true });
         this.runId = core.getInput('run-id', { required: true });
         this.environment = core.getInput('environment', { required: true });
@@ -38,17 +39,18 @@ class Client {
     }
 
     createRequestBody() {
+        let [, projectName] = this.repository.split('/')
         return {
             title: this.title,
             icon: this.icon,
-            message: this.message,
+            message: `${ this.capitalizeWords(projectName) } ${ this.capitalizeWords(this.environment) }`,
             alertMembers: this.alertMembers ? this.alertMembers.split(',').reduce((acc, curr) => `${ acc } <at id=${ curr }></at>`) : '<at id=all></at>',
             date: this.dateCover(),
-            duration: this.duration,
+            duration: this.calcDuration(),
             changes: this.changes,
-            logUrl: this.logUrl,
+            logUrl: `https://github.com/${ this.repository }/actions/runs/${ this.runId }`,
             trigger: this.trigger,
-            projectUrl: this.projectUrl,
+            projectUrl: `https://github.com/${ this.repository }`,
             theme: {
                 success: 'green',
                 failure: 'red',
@@ -58,15 +60,25 @@ class Client {
     }
 
     dateCover() {
-        let date = new Date(this.timestamp);
-        const year = date.getFullYear();
-        const month = (date.getUTCMonth() + 1).toString().padStart(2, '0');
-        const day = date.getDate().toString().padStart(2, '0');
-        const hours = date.getHours().toString().padStart(2, '0');
-        const minutes = date.getMinutes().toString().padStart(2, '0');
-        const seconds = date.getSeconds().toString().padStart(2, '0');
+        const year = this.startTime.getFullYear();
+        const month = (this.startTime.getUTCMonth() + 1).toString().padStart(2, '0');
+        const day = this.startTime.getDate().toString().padStart(2, '0');
+        const hours = this.startTime.getHours().toString().padStart(2, '0');
+        const minutes = this.startTime.getMinutes().toString().padStart(2, '0');
+        const seconds = this.startTime.getSeconds().toString().padStart(2, '0');
         return `${ year }-${ month }-${ day } ${ hours }:${ minutes }:${ seconds }`;
     }
+
+    calcDuration() {
+        return `${ this.endTime - this.startTime }`;
+    }
+
+    capitalizeWords(str) {
+        return str.split(' ').map(word => {
+            return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+        }).join(' ');
+    }
+
 }
 
 new Client().main().catch(e => core.setFailed(e));
