490b85c673ab4e51dab2e096339a2b7479fbfd02
felixliu <shenghuntianlang@outlook.com>
2024-09-25 18:19:41 +0800

    add build image to acr operator

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,6 +18,6 @@ for var in $(env | grep '^CONFIG_MAP_' | cut -d= -f1); do
     fi
 done
 
-docker buildx build --platform linux/amd64 $DOCKER_BUILD_ARGS -t "$FINAL_IMAGE_URL" -f "$PROJECT_ROOT_DIR/Dockerfile" --push .
+docker buildx build --platform linux/amd64 $DOCKER_BUILD_ARGS -t "$FINAL_IMAGE_URL" -f ./Dockerfile --push .
 
 echo "Docker build completed and image pushed."
