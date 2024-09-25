ca6fee5f28b6d19d86751a286de4584ef7e1bafe
felixliu <shenghuntianlang@outlook.com>
2024-09-25 17:33:44 +0800

    add build image to acr operator

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,6 @@
 # Fail on errors
 set -e
 
-echo "$PROJECT_ROOT_DIR"
-
 # Docker build arguments placeholder
 DOCKER_BUILD_ARGS=""
 
@@ -13,12 +11,12 @@ for var in $(env | grep '^CONFIG_MAP_' | cut -d= -f1); do
     value=$(printenv "$var")
     if [ -n "$value" ]; then
         # Append to Docker build arguments
-        DOCKER_BUILD_ARGS="$DOCKER_BUILD_ARGS --build-arg $var='$value'"
+        DOCKER_BUILD_ARGS="$DOCKER_BUILD_ARGS --build-arg $var=\"$value\""
         echo "Added build argument: --build-arg $var=$value"
     fi
 done
 
 # Execute the Docker buildx build command
-docker buildx build"$DOCKER_BUILD_ARGS" --platform linux/amd64 -t "$FINAL_IMAGE_URL" -f "$PROJECT_ROOT_DIR/Dockerfile" --push .
+docker buildx build --platform linux/amd64"$DOCKER_BUILD_ARGS" -t "$FINAL_IMAGE_URL" -f "$PROJECT_ROOT_DIR/Dockerfile" --push .
 
 echo "Docker build completed and image pushed."
