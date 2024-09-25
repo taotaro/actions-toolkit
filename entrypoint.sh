662237ca6c7bf0dea586a3269f72dbc95a81829d
felixliu <shenghuntianlang@outlook.com>
2024-09-25 18:08:42 +0800

    add build image to acr operator

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ for var in $(env | grep '^CONFIG_MAP_' | cut -d= -f1); do
         # Remove the 'CONFIG_MAP_' prefix from the variable name
         stripped_var_name=$(echo "$var" | sed 's/^CONFIG_MAP_//')
         # Append the stripped variable to Docker build arguments
-        DOCKER_BUILD_ARGS="$DOCKER_BUILD_ARGS --build-arg $stripped_var_name=\"$value\""
+        DOCKER_BUILD_ARGS="$DOCKER_BUILD_ARGS --build-arg $stripped_var_name=$value"
         echo "Added build argument: --build-arg $stripped_var_name=$value"
     fi
 done
