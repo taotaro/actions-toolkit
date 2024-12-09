aeb4184a986bfe91f4bcec3de736106b68b9986c
felixliu <shenghuntianlang@outlook.com>
2024-12-09 17:26:42 +0800

    add build image to acr operator

--- dist/index.js
+++ dist/index.js
@@ -39240,7 +39240,7 @@ class Client {
             // print error message
             console.log(error.message);
             // Please click on the link below for diagnosis.
-            console.log(error.data['Recommend']);
+            console.log(error.data);
             Util.default.assertAsString(error.message);
         }
     }
